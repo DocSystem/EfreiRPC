f4beea44f93763acf35b6e1e71e09a1221603e91
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-09-07 15:35:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from pypresence import Presence
+import requests
+import icalendar
+import datetime
+import time
+import pytz
+import json
+
+try:
+    with open("config.json", "r") as f:
+        CONFIG = json.load(f)
+except FileNotFoundError:
+    import setup
+    CONFIG = setup.CONFIG
+
+DISCORD_APPID = "1016321832992903168"
+
+
+def get_agenda():
+    r = requests.get(CONFIG["agenda_url"])
+    cal = icalendar.Calendar.from_ical(r.content)
+    return cal
+
+
+def get_current_event(calendar):
+    current_utc = pytz.UTC.localize(datetime.datetime.utcnow())
+    events = []
+    for event in calendar.walk():
+        if event.name == "VEVENT":
+            start = event.get("dtstart").dt
+            end = event.get("dtend").dt
+            summary = event.get("summary")
+            location = event.get("location")
+            description = event.get("description")
+            events.append(
+                {"start": start, "end": end, "nom_cours": summary, "salle": location, "nom_prof": description})
+    for event in events:
+        if event["start"] < current_utc < event["end"]:
+            return event
+    return None
+
+
+def show_presence(event):
+    RPC.update(details=event[CONFIG["first_key"]], state=event[CONFIG["second_key"]], large_image=CONFIG["large_icon"], large_text=CONFIG["large_icon_text"], start=int(time.mktime(event["start"].timetuple())) + 3600, end=int(time.mktime(event["end"].timetuple())) + 3600)
+
+
+def hide_presence():
+    RPC.clear()
+
+
+print("Démarrage de la Rich Presence Discord...")
+RPC = Presence(DISCORD_APPID)
+RPC.connect()
+print("Rich Presence connectée !")
+
+print("Téléchargement du Planning...")
+cal = get_agenda()
+print("Planning téléchargé !")
+
+print("Démarrage de l'actualisation de la Rich Presence...")
+while True:
+    curr_event = get_current_event(cal)
+    if curr_event is not None:
+        show_presence(curr_event)
+    else:
+        hide_presence()
+    time.sleep(15)
---
+++ setup.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+CONFIG = {}
+
+
+def setup_planning():
+    print("======== PLANNING MYEFREI ========")
+    print("Veuillez vous rendre sur votre planning MyEfrei (https://www.myefrei.fr/portal/student/planning)")
+    print("Et cliquer sur le bouton \"COPIER URL PLANNING (ICAL)\"")
+    CONFIG["agenda_url"] = input("Puis collez le lien ici : ").strip().replace("webcal://", "https://")
+    r = requests.get(CONFIG["agenda_url"])
+    if r.status_code != 200:
+        print("Erreur : Le lien que vous avez entré n'est pas valide.")
+        setup_planning()
+    print(" ")
+
+
+def setup_discord():
+    print("======== DISCORD RPC ========")
+    print("Veuillez choisir les données que vous souhaitez afficher sur votre profil Discord.")
+    print("1. Nom du cours")
+    print("2. Nom du professeur")
+    print("3. Salle")
+    CONFIG["first_key"] = ["nom_cours", "nom_prof", "salle"][int(input("1ère ligne : ").strip()) - 1]
+    CONFIG["second_key"] = ["nom_cours", "nom_prof", "salle"][int(input("2ème ligne : ").strip()) - 1]
+    print(" ")
+    print("Veuillez choisir l'icône que vous souhaitez afficher sur votre profil Discord.")
+    print("1. Efrei blanc")
+    print("2. Efrei couleur")
+    print("3. Efrei couleur sur fond blanc")
+    CONFIG["large_icon"] = ["efrei_white", "efrei_color", "efrei_color_bg"][int(input("Icône : ").strip()) - 1]
+    CONFIG["large_icon_text"] = input("Texte de l'icône : ").strip()
+    print(" ")
+
+
+print("Bienvenue sur Efrei Discord RPC !")
+print("Il semble que ce soit la première fois que vous lancez EfreiRPC.")
+print("La configuration va maintenant commencer.")
+print(" ")
+setup_planning()
+setup_discord()
+print("Configuration terminée !")
+print(" ")
+with open("config.json", "w") as f:
+    json.dump(CONFIG, f)
